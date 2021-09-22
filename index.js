68c5c8c1f1422061809996f5385e2d05aedf861a
Rafael Affonso <hrafaelaf@gmail.com>
2021-09-22 17:29:11 -0300

    feat: create main file

---
+++ index.js
@@ -0,0 +1,28 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run() {
+    const token = core.getInput('token');
+    const env = core.getInput('envirnment');
+    const type = core.getInput('type');
+    const ref = github.context.ref;
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+
+    // Get octokit client for making API calls
+    const octokit = github.getOctokit(token)
+
+    const tags = await octokit.request(`GET /repos/${owner}/${repo}/git/tags/`)
+    console.log(tags)
+
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+}
+
+try {
+    run()
+} catch (error) {
+    core.setFailed(error.message);
+}
